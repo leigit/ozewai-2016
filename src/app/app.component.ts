390f98045b97b026533dae72ab1e7c419d1c37dd
Leigh Gittoes <leighgittoes@gmail.com>
2016-11-27 14:30:02 +1100

    modal component with tab trap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ModalComponent } from './modal/modal.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
+
 export class AppComponent {
   title = 'app works!';
+  @ViewChild(ModalComponent)
+  public readonly modal: ModalComponent;
+
 }
---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements OnInit {
+
+  public visible = false;
+   private visibleAnimate = false;
+
+   public show(): void {
+     this.visible = true;
+     setTimeout(() => this.visibleAnimate = true);
+   }
+
+   public hide(): void {
+     this.visibleAnimate = false;
+     setTimeout(() => this.visible = false, 300);
+   }
+
+  constructor() { }
+
+  ngOnInit() {
+
+      var previouslyFocused = document.activeElement;
+      var modal        = <HTMLElement>document.querySelector('.modal');
+      modal.addEventListener('keydown', trapTabKey);
+      let focusableElements = modal.querySelectorAll('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]');
+
+      focusableElements = Array.prototype.slice.call(focusableElements);
+
+      let firstItem = <HTMLElement>focusableElements[0];
+      let lastItem = <HTMLElement>focusableElements[focusableElements.length -1];
+
+      firstItem.focus();
+
+      function trapTabKey(e) {
+        if (e.keyCode === 9) {
+          // Shift Tab
+          if (e.shiftKey) {
+            if (document.activeElement === firstItem) {
+              e.preventDefault();
+              lastItem.focus();
+            }
+          // Tab
+          } else {
+            if (document.activeElement === lastItem) {
+              e.preventDefault();
+              firstItem.focus();
+            }
+          }
+        } else if (e.keyCode === 27) {
+          close();
+        }
+      }
+  }
+
+}
